2faf9fd8fa00cd571a33b60f63e83ded03bf2ea8
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2022-12-06 03:24:37 +0900

    react를 이용한 To Do List(1)
    배열 추가 방법 공부 (...array)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  return;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); //기본이벤트를 작동하지 못하게함 form이면 새로고침 작동x
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    //배열앞에 ...을 쓰면 현재 있는 배열에 추가할 수 있다.
+    setToDo("");
+  };
+  console.log(toDos);
+
+  return (
+    <div>
+      <h1>My ToDos ({toDos.length}개)</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          type="text"
+          placeholder="to do 입력"
+          onChange={onChange}
+        />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
