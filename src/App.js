a7cae9d3a30b527c5c554db66e53545203470ffd
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2022-12-09 23:06:58 +0900

    react를 이용한 To Do List(2)
    map함수를 이용하여 배열 안에 있는 각각의 element를 바꿀 수 있게 함

--- src/App.js
+++ src/App.js
@@ -27,6 +27,22 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+        {/* map함수는 배열 안에 있는 각각의 element를
+        바꿀 수 있게 해준다. map()의 ()에는 함수를 넣을 수 있는데
+        배열의 모든 item에 대해 실행된다 
+        toDos는 단순한 string으로 구성된 배열인데 그것을 map함수를 써서
+        새로운 배열 즉, li로 구성된 배열로 바꿈
+        리액트는 기본적으로 list에 있는 모든 item을 인식하기 때문에 key를 넣어 
+        고유하게 만들어줘야된다 map함수의 첫번째는 값,두번째는 index=숫자를 의미한다
+        그래서 toDos.map((item, index))을 해줘서 li에 key값을 넣는다
+        최종적으로 map함수를 썼을때 모습은  <li key={0}>{item}</li>,<li key={2}>{item}</li>
+        반복.... */}
+      </ul>
     </div>
   );
 }
