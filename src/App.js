02fbeed20885a19e1b1c8807e95bbe88eb9867cd
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-02-09 00:18:35 +0900

    React Router
    react-router-dom을 설치하고
    Route 컴포넌트를 이용하여 페이지를 바꾸게 해줌
    Link 컴포넌트를 이용하여 특정버튼을 클릭시 새로고침없이 이동할 수 있게 함

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+
+import Home from "./routes/Home";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 export default App;
 
-/* 다른 요소들이 리렌더링되어도 api는 처음 한번만 불러올 수 있게 
-        useEffect를 써준다 
-        1.fetch로 코인 데이터를 불러온다 (text형식)
-        2.then함수를 써서 브라우저가 객체를 인식할 수 있는 json으로 데이터를 변환해준다
-        3..then((json) => setMovies(json))== javascript가 response(데이터)를 
-        JSON형식으로 변환해주면 변환된 데이터를 json이라는 변수에 담고, setMovies에 json(변환된데이터)를 담는것.*/
+/* react-router-dom 5버전 -> 버전6 바뀐 부분
 
-/* fetch-then을 대신하여 async-await를 보편적으로 사용한다 
+1. Switch컴포넌트가 Routes컴포넌트로 대체되었습니다.
+Switch -> Routes
 
-const response = await fetch(`링크`); 링크 데이터를 불러옴
-const json = await response.json();   링크 데이터를 json으로 바꿈
+2. Route컴포넌트 사이에 자식 컴포넌트를 넣지 않고, element prop에 자식 컴포넌트를 할당하도록 바뀌었습니다.
+Route path="/" element={< Home / >}
 
-response변수를 지정하고 json을 다시 바꿔줘야되서 변수 선언을 2번 해야되는데 
-위 2문장을 
-==>
-const json = await(await fetch(`링크`).json());으로 await를 await로 감사서
-한문장으로 간편하게 바꿀 수도 있다.
-*/
+react-router-dom 6버전 문서
+https://reactrouter.com/docs/en/v6/getting-started/overview */
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,12 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({ medium_cover_image, title, summary, genres }) {
   return (
     <div>
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -27,3 +30,6 @@ export default Movie;
 
 /* prototype는 첫문자가 소문자지만
 카멜표기법에 의해 변수명을 쓸때에는 대문자로 쓰기때문에 주의할것 */
+/* 리액트dom에 있는 Link 컴포넌트를 이용해서 html의 a를 대체해서 사용한다 
+EX)  <Link to = "/movie"> 내용 </Link>    
+*/
